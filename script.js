fa238f19ad0f4da1a6a675501f7c990936fe79a4
Joao Vitor Couto <joaovitorcouto.ifsul@gmail.com>
2022-07-01 09:59:08 -0300

    :sparkels: basics functions ready

--- script.js
+++ script.js
@@ -22,49 +22,64 @@ const buttonCalculator_reset = document.querySelector('.reset');
 
 let number = '';
 let operation = [];
+let cont;
 let result = 0;
 let firstTime = true;
 let sum = false;
 let subtracion = false;
+let division = false;
+let percentage = false;
 let multiplication = false;
 
 buttonCalculator_1.addEventListener('click', () => {
     number += '1'
+    inputValue.value = number
 })
 buttonCalculator_2.addEventListener('click', () => {
     number += '2'
+    inputValue.value = number
 })
 buttonCalculator_3.addEventListener('click', () => {
     number += '3'
+    inputValue.value = number
 })
 buttonCalculator_4.addEventListener('click', () => {
     number += '4'
+    inputValue.value = number
 })
 buttonCalculator_5.addEventListener('click', () => {
     number += '5'
+    inputValue.value = number
 })
 buttonCalculator_6.addEventListener('click', () => {
     number += '6'
+    inputValue.value = number
 })
 buttonCalculator_7.addEventListener('click', () => {
     number += '7'
+    inputValue.value = number
 })
 buttonCalculator_8.addEventListener('click', () => {
     number += '8'
+    inputValue.value = number
 })
 buttonCalculator_9.addEventListener('click', () => {
     number += '9'
+    inputValue.value = number
 })
 buttonCalculator_0.addEventListener('click', () => {
     number += '0'
+    inputValue.value = number
 })
 buttonCalculator_ponto.addEventListener('click', () => {
     number += '.'
+    inputValue.value = number
 })
 buttonCalculator_sum.addEventListener('click', () => {
     if (number != '') {
         operation.push(convertToNumber(number))
         number = ''
+        inputValue.value = ''
     }
     sum = true
 })
@@ -72,6 +87,7 @@ buttonCalculator_subtracion.addEventListener('click', () => {
     if (number != '') {
         operation.push(convertToNumber(number))
         number = ''
+        inputValue.value = ''
     }
     subtracion = true
 })
@@ -79,17 +95,32 @@ buttonCalculator_multiplication.addEventListener('click', () => {
     if (number != '') {
         operation.push(convertToNumber(number))
         number = ''
+        inputValue.value = ''
     }
     multiplication = true
 })
 buttonCalculator_division.addEventListener('click', () => {
-
+    if (number != '') {
+        operation.push(convertToNumber(number))
+        number = ''
+        inputValue.value = ''
+    }
+    division = true
 })
 buttonCalculator_percentage.addEventListener('click', () => {
-
+    if (number != '') {
+        operation.push(convertToNumber(number))
+        number = ''
+        inputValue.value = ''
+    }
+    percentage = true
 })
 buttonCalculator_reset.addEventListener('click', () => {
-
+    number = ''
+    result = 0
+    firstTime = true
+    inputValue.value = ''
+    console.log("Calculator clean")
 })
 buttonCalculator_equality.addEventListener('click', () => {
     if (number != '') {
@@ -121,8 +152,43 @@ buttonCalculator_equality.addEventListener('click', () => {
             result *= value
         }
         multiplication = false
+    } else if (division) {
+        for (let value of operation) {
+            if (firstTime) {
+                result = operation[0] * operation[0]
+                firstTime = false
+            }
+            result /= value
+        }
+        division = false
+    } else if (percentage) {
+
+        let percentageCorrectionFactor = '1'
+
+        if (result != 0) {
+            cont = 0;
+        } else {
+            cont = 1;
+        }
+
+        while (cont < operation.length) {
+            cont++;
+            percentageCorrectionFactor = percentageCorrectionFactor + '00'
+        }
+
+        for (let value of operation) {
+            if (firstTime) {
+                result = 1
+                firstTime = false
+            }
+            result *= value
+        }
+        percentageCorrectionFactor = Number(percentageCorrectionFactor)
+        result = result / percentageCorrectionFactor
+        percentage = false
     }
     operation = []
+    inputValue.value = result
     console.log("resultado: " + result)
 })
 
